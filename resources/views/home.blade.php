4ac6e443d89a63281e4848f19499594fe9fbfd1f
Paolo Giovannini <giovannini13@gmail.com>
2023-05-10 15:54:03 +0200

    jumbotron e lista fumetti tramite route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,24 @@
 @section('page-title', 'Home page')
 
 @section('content')
-    <h1>contenuto main</h1>
+
+    <div id="jumbotron">
+    </div>
+    <div class="wrap-up">
+        <div class="container">
+          <span class="button">Current Series</span>
+        </div>
+        <div id="container-fumetti" class="container d-flex">
+            @foreach ($comics as $comic)
+                <div class="card">
+                    <img src="{{$comic['thumb']}}" alt="">
+                    <p>{{$comic['series']}}</p>
+                </div>
+            @endforeach
+          
+        </div>
+        <div class="loading">
+          <span>LOAD MORE</span>
+        </div>
+      </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-}) ->name('home');
+    $comics = config('comics.comics_list');
+    return view('home', compact('comics'));
+})->name('home');
